bdd32c3eab0f6ee540cadf8f63b2339995edb066
Bivvercode <aaandersson98@gmail.com>
2024-04-21 00:22:31 +0200

    Added CustomerUser in models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomerUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    shipping_address = models.TextField(blank=True)
+    billing_address = models.TextField(blank=True)
