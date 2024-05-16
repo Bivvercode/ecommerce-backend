53cf3991909e7d194d753bcbb7b2adbf20be1311
Bivvercode <aaandersson98@gmail.com>
2024-05-17 00:26:54 +0200

    Added more tests for register user and fixed the serializer to include more validation

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from .models import CustomerUser
 
 
@@ -9,13 +10,26 @@ class CustomerUserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'first_name', 'last_name', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate(self, attrs):
+        required_fields = ['username', 'email',
+                           'first_name', 'last_name',
+                           'password']
+        for field in required_fields:
+            if field not in attrs:
+                raise serializers.ValidationError(
+                    {field: f'{field} field is required.'}
+                )
+        password = attrs.get('password')
+        user = CustomerUser(**attrs)
+        try:
+            validate_password(password, user)
+        except ValidationError as e:
+            raise serializers.ValidationError({'password': e.messages})
+        return attrs
+
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = CustomerUser(**validated_data)
-        if password:
-            validate_password(password, user)
-            user.set_password(password)
-        else:
-            raise serializers.ValidationError("Password field is required")
+        user.set_password(password)
         user.save()
         return user
--- users/tests/test_register_user_view.py
+++ users/tests/test_register_user_view.py
@@ -23,3 +23,67 @@ class TestRegisterView:
         assert response.status_code == 201
         assert 'token' in response.data
         assert CustomerUser.objects.filter(username='testuser').exists()
+
+    @pytest.mark.django_db
+    def test_register_user_no_password(self):
+        """Test registering a new user without a password."""
+        client = APIClient()
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'email': 'test@email.com',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == 400
+        assert 'password' in response.data
+        assert not CustomerUser.objects.filter(username='testuser').exists()
+
+    @pytest.mark.django_db
+    def test_register_user_no_username(self):
+        """Test registering a new user without a username."""
+        client = APIClient()
+        url = reverse('register')
+        data = {
+            'password': 'StrongPassword123!',
+            'email': 'test@email.com',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == 400
+        assert 'username' in response.data
+        assert not CustomerUser.objects.filter(email='test@email.com').exists()
+
+    @pytest.mark.django_db
+    def test_register_user_no_email(self):
+        """Test registering a new user without an email."""
+        client = APIClient()
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'password': 'StrongPassword123!',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == 400
+        assert 'email' in response.data
+        assert not CustomerUser.objects.filter(username='testuser').exists()
+
+    @pytest.mark.django_db
+    def test_register_user_no_first_name(self):
+        """Test registering a new user without a first name."""
+        client = APIClient()
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'password': 'StrongPassword123!',
+            'email': 'test@email.com',
+            'last_name': 'User'
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == 400
+        assert 'first_name' in response.data
+        assert not CustomerUser.objects.filter(username='testuser').exists()
