904d141f724262fb8f7fa4523b13568b1582cefc
Bivvercode <aaandersson98@gmail.com>
2024-05-17 22:31:33 +0200

    Added url to login

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from .views import authentication
 
 urlpatterns = [
     path('register/', authentication.RegisterView.as_view(), name='register'),
+    path('login/', authentication.LoginView.as_view(), name='login'),
 ]
