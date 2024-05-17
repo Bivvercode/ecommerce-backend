a792a488dce04bee105a9d9e311ace92a24726e9
Bivvercode <aaandersson98@gmail.com>
2024-05-17 22:21:56 +0200

    Added test checking if user password has lowercase character

--- users/tests/test_register_user_view.py
+++ users/tests/test_register_user_view.py
@@ -223,6 +223,27 @@ class TestRegisterView:
         ).exists()
 
     @pytest.mark.django_db
+    def test_register_user_no_lowercase_password(self):
+        """
+        Test registering a new user with a password not containing lowercase.
+        """
+        client = APIClient()
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'password': 'WEAKPASSWORD1!',
+            'email': 'test@gmail.com',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        response = client.post(url, data, format='json')
+        assert response.status_code == 400
+        assert 'password' in response.data
+        assert not CustomerUser.objects.filter(
+            username='testuser'
+        ).exists()
+
+    @pytest.mark.django_db
     def test_register_user_no_special_char_password(self):
         """
         Test registering a new user with a password
