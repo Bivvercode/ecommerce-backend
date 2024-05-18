56f95080d8aeed4050c33e92b6f80b744ff97993
Bivvercode <aaandersson98@gmail.com>
2024-05-18 14:43:26 +0200

    Created an url for profileview

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import authentication
+from .views import authentication, profile
 
 urlpatterns = [
     path('register/', authentication.RegisterView.as_view(), name='register'),
     path('login/', authentication.LoginView.as_view(), name='login'),
     path('logout/', authentication.LogoutView.as_view(), name='logout'),
+
+    path('profile/', profile.ProfileView.as_view(), name='profile'),
 ]
