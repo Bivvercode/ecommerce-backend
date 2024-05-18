c1a44dda2eae71e4522770a41e4fbe7a816535d9
Bivvercode <aaandersson98@gmail.com>
2024-05-18 11:24:54 +0200

    Created url for logout

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from .views import authentication
 urlpatterns = [
     path('register/', authentication.RegisterView.as_view(), name='register'),
     path('login/', authentication.LoginView.as_view(), name='login'),
+    path('logout/', authentication.LogoutView.as_view(), name='logout'),
 ]
