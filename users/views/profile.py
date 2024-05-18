27d3cbbe5c5a1c2aaf930577ccc19578658e15da
Bivvercode <aaandersson98@gmail.com>
2024-05-18 14:41:00 +0200

    Added a function to change profile info for customer user

---
+++ users/views/profile.py
@@ -0,0 +1,29 @@
+from rest_framework import views, permissions, status
+from rest_framework.response import Response
+from users.models import CustomerUser
+
+
+class ProfileView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Implement profile retrieval logic here
+        pass
+
+    def put(self, request):
+        user: CustomerUser = request.user
+        data = request.data
+
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        user.email = data.get('email', user.email)
+        user.phone_number = data.get('phone_number', user.phone_number)
+        user.shipping_address = data.get('shipping_address',
+                                         user.shipping_address)
+        user.billing_address = data.get('billing_address',
+                                        user.billing_address)
+
+        user.save()
+
+        return Response({"detail": "Profile updated"},
+                        status=status.HTTP_200_OK)
