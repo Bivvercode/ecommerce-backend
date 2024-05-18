4a2adfdaeed643983334cafd037d66088779e608
Bivvercode <aaandersson98@gmail.com>
2024-05-18 15:02:42 +0200

    Added url for change password

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import authentication, profile
+from .views import authentication, profile, passwords
 
 urlpatterns = [
     path('register/', authentication.RegisterView.as_view(), name='register'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('logout/', authentication.LogoutView.as_view(), name='logout'),
 
     path('profile/', profile.ProfileView.as_view(), name='profile'),
+
+    path('password/change/', passwords.ChangePasswordView.as_view(),
+         name='change_password'),
 ]
