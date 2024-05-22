6274a7ac7e3940bd8c885444d9ac14127c74a8d3
Bivvercode <aaandersson98@gmail.com>
2024-05-22 21:42:33 +0200

    Added serializers for the products models

---
+++ products/serializers.py
@@ -0,0 +1,83 @@
+from rest_framework import serializers
+from .models import (Unit, Category, Product,
+                     ProductCategory, Image, Cart,
+                     CartItem, Wishlist, Order,
+                     OrderItem)
+
+
+class UnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Unit
+        fields = ['id', 'name', 'symbol']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'parent']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    unit = serializers.PrimaryKeyRelatedField(read_only=True)
+    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price',
+                  'discount', 'unit', 'quantity_per_unit',
+                  'currency', 'categories']
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(read_only=True)
+    category = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'product', 'category']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Image
+        fields = ['id', 'image_file', 'product']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'created_at']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    cart = serializers.PrimaryKeyRelatedField(read_only=True)
+    product = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'cart', 'product', 'quantity']
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    products = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'user', 'products']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'total_price', 'status', 'created_at']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    order = serializers.PrimaryKeyRelatedField(read_only=True)
+    product = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'order', 'product', 'quantity', 'price', 'discount']
