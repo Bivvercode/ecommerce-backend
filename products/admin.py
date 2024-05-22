b6c3002f55b0f8bc87e82e577e1a05da0c5285e1
Bivvercode <aaandersson98@gmail.com>
2024-05-22 21:12:04 +0200

    Registered models in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (Unit, Category, Product, ProductCategory,
+                     Image, Cart, CartItem,
+                     Wishlist, Order, OrderItem)
 
-# Register your models here.
+admin.site.register(Unit)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(Image)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Wishlist)
+admin.site.register(Order)
+admin.site.register(OrderItem)
