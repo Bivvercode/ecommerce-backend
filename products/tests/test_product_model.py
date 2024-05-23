bf89fa0f0bd06ab885c5138c8232addb80769656
Bivvercode <aaandersson98@gmail.com>
2024-05-24 00:42:56 +0200

    Created test checking negative price on product

--- products/tests/test_product_model.py
+++ products/tests/test_product_model.py
@@ -90,3 +90,17 @@ class TestProductModel:
                 quantity_per_unit=1.00,
                 currency='USD'
             )
+
+    @pytest.mark.django_db
+    def test_product_price_cannot_be_negative(self, setup_data):
+        unit, _ = setup_data
+        with pytest.raises(ValidationError):
+            Product.objects.create(
+                name='Test Product',
+                description='This is a test product',
+                price=-100.00,
+                discount=10,
+                unit=unit,
+                quantity_per_unit=1.00,
+                currency='USD'
+            )
