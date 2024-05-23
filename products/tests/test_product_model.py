806af7d3678aeffe87d0b25c6e2d9a589d3f70bf
Bivvercode <aaandersson98@gmail.com>
2024-05-24 00:38:03 +0200

    Created test checking max 200 char on product name

--- products/tests/test_product_model.py
+++ products/tests/test_product_model.py
@@ -48,3 +48,31 @@ class TestProductModel:
                 quantity_per_unit=1.00,
                 currency='USD'
             )
+
+    @pytest.mark.django_db
+    def test_product_name_cannot_exceed_200_characters(self, setup_data):
+        unit, _ = setup_data
+        with pytest.raises(ValidationError):
+            Product.objects.create(
+                name='a' * 201,
+                description='This is a test product',
+                price=100.00,
+                discount=10,
+                unit=unit,
+                quantity_per_unit=1.00,
+                currency='USD'
+            )
+
+        assert Product.objects.count() == 0
+
+        Product.objects.create(
+            name='a' * 200,
+            description='This is a test product',
+            price=100.00,
+            discount=10,
+            unit=unit,
+            quantity_per_unit=1.00,
+            currency='USD'
+        )
+
+        assert Product.objects.count() == 1
