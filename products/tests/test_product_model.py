985553c64e80042541a6ff298a0dbb0e7952b821
Bivvercode <aaandersson98@gmail.com>
2024-05-24 00:58:48 +0200

    Created test to check that category is optional

--- products/tests/test_product_model.py
+++ products/tests/test_product_model.py
@@ -187,3 +187,18 @@ class TestProductModel:
                 quantity_per_unit=1.00,
                 currency='USDD'
             )
+
+    @pytest.mark.django_db
+    def test_product_categories_are_optional(self, setup_data):
+        unit, _ = setup_data
+        product = Product.objects.create(
+            name='Test Product',
+            description='This is a test product',
+            price=100.00,
+            discount=10,
+            unit=unit,
+            quantity_per_unit=1.00,
+            currency='USD'
+        )
+        assert product.categories.count() == 0
+        assert Product.objects.count() == 1
