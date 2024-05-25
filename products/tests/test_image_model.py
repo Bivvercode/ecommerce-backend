67867aedfdb3125f54357ed6fd1e96cbce9784ca
Bivvercode <aaandersson98@gmail.com>
2024-05-25 18:56:43 +0200

    Created test cases for the image model in products

---
+++ products/tests/test_image_model.py
@@ -0,0 +1,67 @@
+import os
+from pathlib import Path
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
+from products.models import Image, Product, Unit, Category, ProductCategory
+
+
+class TestImageModel:
+    '''Test cases for the Image model.'''
+
+    @pytest.fixture
+    def unit(self):
+        return Unit.objects.create(name='Kilogram', symbol='kg')
+
+    @pytest.fixture
+    def category(self):
+        return Category.objects.create(name='Electronics')
+
+    @pytest.fixture
+    def product(self, unit, category):
+        product = Product.objects.create(
+            name='Test Product',
+            description='This is a test product',
+            price=100.00,
+            discount=10,
+            unit=unit,
+            quantity_per_unit=1.00,
+            currency='USD'
+        )
+        ProductCategory.objects.create(product=product, category=category)
+        return product
+
+    @pytest.fixture
+    def image_file(self, tmp_path):
+        file = tmp_path / "test_image.jpg"
+        file.write_bytes(b'')
+        uploaded_file = SimpleUploadedFile(name='test_image.jpg',
+                                           content=Path(file).read_bytes(),
+                                           content_type='image/jpeg')
+
+        yield uploaded_file
+
+        os.remove(os.path.join(settings.MEDIA_ROOT,
+                               'product_images',
+                               uploaded_file.name))
+
+    @pytest.mark.django_db
+    def test_create_image(self, product, image_file):
+        image = Image.objects.create(image_file=image_file, product=product)
+        assert Image.objects.count() == 1
+        assert image.image_file.name == 'product_images/test_image.jpg'
+        assert image.product == product
+
+    @pytest.mark.django_db
+    def test_delete_image(self, product, image_file):
+        image = Image.objects.create(image_file=image_file, product=product)
+        assert Image.objects.count() == 1
+        image.delete()
+        assert Image.objects.count() == 0
+
+    @pytest.mark.django_db
+    def test_product_delete_cascades(self, product, image_file):
+        Image.objects.create(image_file=image_file, product=product)
+        assert Image.objects.count() == 1
+        product.delete()
+        assert Image.objects.count() == 0
