70b89a5dbc586803f6bd7e37f41bf5e37ed3267a
Bivvercode <aaandersson98@gmail.com>
2024-05-30 13:35:58 +0200

    Added docstring to users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,15 @@
+"""
+This module defines the URL routes for the 'users' Django app.
+
+The routes include:
+    - Register: A route for user registration.
+    - Login: A route for user login.
+    - Logout: A route for user logout.
+    - Profile: A route for retrieving and updating the profile
+               of an authenticated user.
+    - Change Password: A route for changing the password
+                       of an authenticated user.
+"""
 from django.urls import path
 from .views import authentication, profile, passwords
 
