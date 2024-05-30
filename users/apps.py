ca43a11cfca0897a775c45b59da8d8467c4dbd8c
Bivvercode <aaandersson98@gmail.com>
2024-05-30 13:26:59 +0200

    Added docstring to users/apps.py

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,20 @@
+"""
+This module contains the configuration for the 'users' Django app.
+
+Classes:
+    UsersConfig: Configuration for the 'users' Django app.
+"""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    Configuration for the 'users' Django app.
+
+    Attributes:
+        default_auto_field: Specifies the type of auto-created primary key
+                            field for models without an explicit primary key.
+        name: The name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
