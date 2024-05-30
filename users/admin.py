fc46ce120d16ecb04b305fea020645698177da95
Bivvercode <aaandersson98@gmail.com>
2024-05-30 13:25:35 +0200

    Added docstring to users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
+"""
+This module configures the Django admin site for the CustomerUser model.
+
+It registers the CustomerUser model with the admin site, allowing it
+to be managed through the Django admin interface.
+
+Operations:
+    - Register the CustomerUser model with the admin site.
+"""
 from django.contrib import admin
 from .models import CustomerUser
 
