5d009116c4ca673c5c69c440b657c99adb9daab8
Bivvercode <aaandersson98@gmail.com>
2024-06-23 19:43:22 +0200

    Added class docstring to products/apps.py ProductsConfig

--- products/apps.py
+++ products/apps.py
@@ -8,5 +8,13 @@ from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
+    """
+    Configuration for the 'products' Django app.
+
+    Attributes:
+        default_auto_field: Specifies the type of auto-created primary key
+                            field for models without an explicit primary key.
+        name: The name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
