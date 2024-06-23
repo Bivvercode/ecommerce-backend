181bd17c6eeb89817a9f17df60567c4a83947321
Bivvercode <aaandersson98@gmail.com>
2024-06-23 19:44:24 +0200

    Added docstrings to orders/apps.py

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,20 @@
+"""
+This module contains the configuration for the 'orders' Django app.
+
+Classes:
+    OrdersConfig: Configuration for the 'orders' Django app.
+"""
 from django.apps import AppConfig
 
 
 class OrdersConfig(AppConfig):
+    """
+    Configuration for the 'orders' Django app.
+
+    Attributes:
+        default_auto_field: Specifies the type of auto-created primary key
+                            field for models without an explicit primary key.
+        name: The name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
