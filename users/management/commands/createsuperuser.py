fe0e5a9e83f19fde2eae349dd51f55d5db24ccfc
Bivvercode <aaandersson98@gmail.com>
2024-06-24 14:47:22 +0200

    Added docstrings to createsuperuser.py

--- users/management/commands/createsuperuser.py
+++ users/management/commands/createsuperuser.py
@@ -1,3 +1,11 @@
+"""
+This module provides an extension to Django createsuperuser
+command with additional fields.
+
+It allows for the creation of a superuser via the command line,
+prompting for username, email, password, first name, and last name.
+Password input is securely handled to not display the typed characters.
+"""
 import getpass
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
@@ -6,6 +14,16 @@ User = get_user_model()
 
 
 class Command(BaseCommand):
+    """
+    A custom command to create a superuser with username,
+    email, password, first name and last name fields.
+
+    This command extends Django's BaseCommand class and
+    overrides the handle method to include prompts for
+    additional user information. It uses the get_user_model
+    function to support custom user models.
+    """
+
     help = 'Update a superuser with first name and last name.'
 
     def add_arguments(self, parser):
