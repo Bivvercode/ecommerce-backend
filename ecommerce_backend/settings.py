4ddbc6bafa590f1bb5ee07a57b8c19632a148574
Bivvercode <aaandersson98@gmail.com>
2024-07-03 11:38:56 +0200

    Changed in settings to have a media root and url to store images

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -16,6 +16,9 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
